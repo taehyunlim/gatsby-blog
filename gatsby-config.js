bc612d3c96467532146901b1e9b2abe8251ab636
taehyunlim <taehyun.lim@gmail.com>
2019-02-22 11:17:19 -0800

    Favicon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,10 +30,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#CCDFCB`,
+        theme_color: `#CCDFCB`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
