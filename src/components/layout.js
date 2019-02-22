d19fb734dd8d10038fde89645ebae0b68ae4016f
taehyunlim <taehyun.lim@gmail.com>
2019-02-22 13:40:30 -0800

    Moved main component to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Main from './main'
+import Media from 'react-media'
+import Sidebar from "./sidebar"
 import "./layout.css"
 import "./layout-overide.css"
 
@@ -21,7 +22,71 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <Main children={children} siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: "0 auto",
+            maxWidth: 980,
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            justifyContent: "space-between",
+            height: "100%",
+            padding: "1.0875rem"
+          }}>
+          <Media query={{ maxWidth: 848 }}>
+            {matches =>
+              matches ? (
+                <>
+                  <div style={{
+                    flex: 1,
+                    paddingBottom: "25px"
+                  }}>
+                    {children}
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <Sidebar
+                      title="ecomuix"
+                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    />
+                    <Sidebar
+                      title="About tldev"
+                      description="Excepteur sint occaecat cupidatat non proident, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    />
+                  </div>
+                </>
+              ) : (
+                  <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}>
+                    <div style={{ flex: 2, paddingRight: "7%" }}>
+                      {children}
+                    </div>
+                    <div style={{ flex: 1 }}>
+                      <Sidebar
+                        title={data.site.siteMetadata.title}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                      />
+                      <Sidebar
+                        title="About tldev"
+                        description="Excepteur sint occaecat cupidatat non proident, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                      />
+                    </div>
+                  </div>
+                )}
+          </Media>
+          <footer
+            style={{
+              display: "flex",
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "flex-end"
+            }}
+          >
+            tldev © {new Date().getFullYear()}
+          </footer>
+        </div>
       </>
     )}
   />
