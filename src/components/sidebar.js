e6e95fc7111176904c9581acfeaaad679c261cc6
taehyunlim <taehyun.lim@gmail.com>
2019-02-22 17:22:45 -0800

    Additional styling

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,9 +3,9 @@ import React from "react"
 const Sidebar = (props) => (
   <div
     style={{
-      border: '2px solid #e6e6e6',
+      border: '2px solid #e3e3e3',
       maxWidth: 960,
-      padding: '0.5rem',
+      padding: '0.5rem 1rem',
       marginBottom: '25px'
     }}
   >
